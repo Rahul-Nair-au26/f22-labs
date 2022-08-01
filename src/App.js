7ebfe9424ac324b6e4333ff212aad65bbc01005b
Rahul Nair <rahulnair217@gmail.com>
2022-08-01 12:31:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className='navbar'>
+        <span class='Omega-Copy-2'> Omega </span>
+        <span class="Demo-Pages-Support">
+          <span class="text-style-1"> Demo </span> Pages Support
+        </span>
+        <span class="Get-this-app"> Get This App </span>
+        <span class="BG"></span>
+      </div>
+      <div className='introduction'>
+        <img src="img/i-phone-body.png"srcSet='img/i-phone-body@2x.png 2x, img/i-phone-body@3x.png 3x' class="iphone-Body"> </img>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
